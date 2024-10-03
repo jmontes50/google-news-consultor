2e7fdbcaae548f9da72f12d5c661628afaa64ddb
Jorge Montesinos <osmarmontes50@gmail.com>
2024-10-02 21:54:46 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { google } = require('googleapis');
+const customsearch = google.customsearch('v1');
+
+const API_KEY = process.env.API_KEY;
+const CX = process.env.CX;
+
+async function searchGoogleNews(query) {
+  try {
+    const res = await customsearch.cse.list({
+      q: query,
+      cx: CX,
+      auth: API_KEY,
+      siteSearch: 'news.google.com',
+      num: 10
+    });
+    console.log('Resultados:', res.data.items);
+  } catch (error) {
+    console.error('Error en la búsqueda:', error);
+  }
+}
+
+searchGoogleNews('Incendios Bolivia');
