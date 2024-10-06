354b6177d25c3dc14e80da4b9a88d2c0e76e1896
Jorge Montesinos <osmarmontes50@gmail.com>
2024-10-05 21:51:59 -0500

    added cors2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { searchGoogleNews } = require('./newsService');
+const cors = require('cors');
 const PORT = 3000;
 
 app.use(cors());
